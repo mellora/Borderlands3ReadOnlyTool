32e2d0e5681da9ec5862cb7a2f6c5f8031d79a87
Alexander Mellor <AlexMellor2014@gmail.com>
2020-09-12 15:24:02 -0500

    Modified regex filter
    Now finds only the game saves and filters out the profile save

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CU
 borderlands_3_save_path = os.path.normpath(os.path.join(buf.value, "my games/Borderlands 3/Saved/SaveGames/76561198110445082"))
 
 game_files = [f for f in os.listdir(borderlands_3_save_path) if os.path.isfile(os.path.join(borderlands_3_save_path, f))]
-game_saves = [f for f in game_files if(re.match(r"^\S*.sav$", f) and (re.match(r"")))]
+game_saves = [f for f in game_files if(re.match(r"^\S*(.sav)$", f) and (re.match(r"^((?!profile).)*$", f)))]
 
 if __name__ == '__main__':
     for f in game_saves:
